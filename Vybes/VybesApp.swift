ef040d230fe845af420dff37e52dfad221906e93
Praneeth Vithanage <praneethvithanage10@gmail.com>
2022-09-13 21:46:42 +0530

    Create Project
    New Project Creation

---
+++ Vybes/VybesApp.swift
@@ -0,0 +1,17 @@
+//
+//  VybesApp.swift
+//  Shared
+//
+//  Created by MacBook on 2022-09-13.
+//
+
+import SwiftUI
+
+@main
+struct VybesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
